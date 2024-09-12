64a9a1a6b5c10db1183342e1d4bde0add52a90f4
Jenani Uthayavarnan <jenaniu@hotmail.com>
2024-09-12 11:54:22 -0400

    add links to nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,20 @@
+import { Link } from 'react-router-dom'
 import './NavBar.scss'
 function NavBar() {
     return (
         <>
             <section className="nav-bar">
-                <span className="nav-bar__logo"> That's Our Lingo! </span>
+                <Link to="/" className="nav-bar__logo-link">
+                    <span className="nav-bar__logo"> That's Our Lingo! </span>
+                </Link>
                 <div className="nav-bar__link">
-                    <p className="nav-bar__link--grammar">Grammar</p>
-                    <p className="nav-bar__link--vocab">Vocabulary</p>
+                    <Link to="/" className="nav-bar__link--grammar">
+                        <h2>Grammar</h2>
+                    </Link>
+
+                    <Link to="/" className="nav-bar__link--vocab">
+                        <h2>Vocabulary</h2>
+                    </Link>
                 </div>
             </section>
         </>
