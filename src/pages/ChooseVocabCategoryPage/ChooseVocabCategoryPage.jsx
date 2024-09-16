68c2c5367d71aa0a3ea5e4711b6f3484f06fd24b
Jenani Uthayavarnan <jenaniu@hotmail.com>
2024-09-16 14:37:10 -0400

    add api to choose vocab categories page

--- src/pages/ChooseVocabCategoryPage/ChooseVocabCategoryPage.jsx
+++ src/pages/ChooseVocabCategoryPage/ChooseVocabCategoryPage.jsx
@@ -3,28 +3,59 @@ import Button from '../../components/Button/Button'
 import './ChooseVocabCategoryPage.scss'
 import PageHeader from '../../components/PageHeader/PageHeader'
 import { useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 
 function ChooseVocabCategoryPage() {
+    const [categories, setCategories] = useState([]);
     const navigate = useNavigate()
 
     const handleClick = () => {
-        navigate('/');
+        navigate('/vocab-categories');
         return
-      };
+    };
+
+    useEffect(() => {
+        const getVocabCategories = async () => {
+            try {
+                const response = await axios.get(
+                    "http://localhost:8080/vocabulary/french"
+                );
+                // const categoryChoices = response.data.map((category) => ({
+                //     id: category.id,
+                //     vocab_category: category.vocab_category
+                // }));
+                setCategories(response.data);
+            }
+            catch (error) {
+                console.error("Error getting vocabulary category data", error);
+            }
+        };
+        getVocabCategories();
+    }, []);
+
 
     return (
         <>
             <section className="choose-vocab" >
-                <PageHeader headerClassName="choose-vocab__header" headerText="Choose a vocabulary category"/>
+                <PageHeader headerClassName="choose-vocab__header" headerText="Choose a vocabulary category" />
                 <article className="choose-vocab__buttons">
                     <figure className="choose-vocab__buttons--wrapper">
-                        <Button buttonClassName="choose-vocab__button" onClick={handleClick} buttonText="Foods" />
-                        <Button buttonClassName="choose-vocab__button" onClick={handleClick} buttonText="Hi & Goodbye" />
+
+
+                  
+                            {categories.map((category) => (
+                                <Button key={category.id} buttonText={category.vocab_category} buttonClassName="choose-vocab__button" onClick={handleClick} />
+                            ))}
+
+
+
+                        {/* <Button buttonClassName="choose-vocab__button" onClick={handleClick} buttonText="Hi & Goodbye" /> */}
                     </figure>
                     <figure className="choose-vocab__buttons--wrapper">
-                        <Button buttonClassName="choose-vocab__button" onClick={handleClick} buttonText="Colours" />
-                        <Button buttonClassName="choose-vocab__button" onClick={handleClick} buttonText="Animals" />
+                        {/* <Button buttonClassName="choose-vocab__button" onClick={handleClick} buttonText="Colours" />
+                        <Button buttonClassName="choose-vocab__button" onClick={handleClick} buttonText="Animals" /> */}
                     </figure>
                 </article>
             </section>
--- src/pages/GrammarVocabPage/GrammarVocabPage.jsx
+++ src/pages/GrammarVocabPage/GrammarVocabPage.jsx
@@ -2,9 +2,16 @@ import { Helmet } from 'react-helmet'
 import Button from '../../components/Button/Button'
 import './GrammarVocabPage.scss'
 import PageHeader from '../../components/PageHeader/PageHeader'
+import { useNavigate } from 'react-router-dom';
 
 
 function GrammarVocabPage() {
+
+    let navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate ('/vocab-categories')
+  }
 
     return (
         <>
@@ -12,8 +19,8 @@ function GrammarVocabPage() {
                 <PageHeader headerClassName="grammar-vocab__header" headerText="Let's learn some French!"/>
                 <h1 className="grammar-vocab__subheader">Choose a Category!</h1>
                 <article className="choose-lang__buttons">
-                    <Button buttonClassName="choose-lang__button" buttonText="Vocabulary" />
-                    <Button buttonClassName="choose-lang__button" buttonText="Grammar" />
+                    <Button buttonClassName="choose-lang__button" buttonText="Vocabulary" onClick={handleClick} />
+                    <Button buttonClassName="choose-lang__button" buttonText="Grammar" onClick={handleClick} />
                 </article>
             </section>
         </>
